8e700fe9a21dc83adc084f471b7fc83724949f5d
Mijian Xu <mijian.xu@smail.nju.edu.cn>
2020-08-26 02:58:10 +0800

    travis CD

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ packages = find_packages()
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "2.1.0"
+VERSION = "2.0.9"
 setup(name='bqmail',
       version=VERSION,
       author='Mijian Xu',

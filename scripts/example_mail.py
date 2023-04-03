1ca31217fa5ef2cde368bfe3c16241c0fef44627
Mijian Xu <gomijianxu@gmail.com>
2023-04-03 17:49:47 +0800

    remove example_mail

--- scripts/example_mail.py
+++
@@ -1,8 +0,0 @@
-from bqmail.mail import BQMail
-from obspy import UTCDateTime
-
-bq = BQMail('gomijianxu@163.com', server='smtp.163.com', password='UYBECNASZQWCDFNF', username='mijian')
-bq.query_events(starttime=UTCDateTime(2009, 6, 1), endtime=UTCDateTime(2019, 1, 1),
-                minmagnitude=5.8, catalog='GCMT', latitude=0, longitude=100, minradius=25, maxradius=95)
-bq.query_stations(network='MS', minlatitude=-6, maxlatitude=6.8, minlongitude=94, maxlongitude=107.4, channel='BH?', location='02')
-bq.send_mail(time_before=0, time_after=1000, arrange='events')
